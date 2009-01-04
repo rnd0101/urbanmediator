239bcf20e0492205aacd7e78403a737dcd55f4fb
rnd0110 <rnd0110@12d5a9be-684f-0410-bc4f-c7d56f0f17bc>
2009-01-04 15:52:26 +0000

    old OL removed

--- urbanmediator/static/ol/lib/OpenLayers/SingleFile.js
+++
@@ -1,9 +0,0 @@
-/* Copyright (c) 2006-2007 MetaCarta, Inc., published under the BSD license.
- * See http://svn.openlayers.org/trunk/openlayers/release-license.txt 
- * for the full text of the license. */
-
-OpenLayers = {
-    singleFile: true
-};
-
-
